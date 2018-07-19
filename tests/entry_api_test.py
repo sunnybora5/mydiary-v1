942fa011c516d9cc0e2882d3659b44412d71b4aa
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-19 13:11:21 +0300

    Version entry api test routes

--- tests/entry_api_test.py
+++ tests/entry_api_test.py
@@ -8,18 +8,18 @@ from app.models import Entry
 class EntryApiTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client(self)
         self.entries = Mock.entries()
         Entry.set_values(self.entries)
+        self.client = app.test_client(self)
 
     def test_it_lists_all_entries(self):
-        response = self.client.get('/entries')
+        response = self.client.get('/api/v1/entries')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
         self.assertEqual(json.loads(response.data), {'entries': self.entries})
 
     def test_it_gets_a_specific_entry(self):
-        response = self.client.get('/entries/4')
+        response = self.client.get('/api/v1/entries/4')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
         # The third index of mock entries has id == 4
@@ -27,22 +27,22 @@ class EntryApiTestCase(unittest.TestCase):
 
     def test_it_creates_new_entries(self):
         new_entry = {'title': 'A title', 'body': 'A body'}
-        response = self.client.post('/entries', data=new_entry)
+        response = self.client.post('/api/v1/entries', data=new_entry)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.mimetype, 'application/json')
         self.assertDictContainsSubset(new_entry, json.loads(response.data))
 
     def test_it_updates_entries(self):
         updates = {'title': 'A new title', 'body': 'A new body'}
-        response = self.client.put('/entries/3', data=updates)
+        response = self.client.put('/api/v1/entries/3', data=updates)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
         self.assertDictContainsSubset(updates, json.loads(response.data))
 
     def test_it_deletes_entries(self):
-        response = self.client.delete('/entries/2')
+        response = self.client.delete('/api/v1/entries/2')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
         all_entries = json.loads(self.client.get('/entries').data)
-        # Ensure that entry with id == 3 no longer exists on the models
+        # Ensure that entry with id == 2 no longer exists on the models
         self.assertFalse(any(entry['id'] == 2 for entry in all_entries))

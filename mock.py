9b0d3276f94b91592e6161559b840e4bc31eb774
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-19 19:39:25 +0300

    Remove parsing of date on mock entries

--- mock.py
+++ mock.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 from utils import full_path
 
 
@@ -9,15 +8,8 @@ class Mock:
 
     @staticmethod
     def entries():
-        entries = Mock.__read_data('entries')
-        for i, entry in enumerate(entries):
-            entries[i]['created_at'] = Mock.__parse_date(entries[i]['created_at'])
-        return entries
+        return Mock.__read_data('entries')
 
     @staticmethod
     def __read_data(resource):
         return json.load(open(full_path(resource + '.json')))
-
-    @staticmethod
-    def __parse_date(date_string):
-        return datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')

6efd35fdfe323ef2b1d7fdef6f34c52e15efb242
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-19 19:36:51 +0300

    Add parse_date method and cleanup full_path method on utils

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
-import os
+from os.path import dirname
+from datetime import datetime
 
-ROOT_DIRECTORY = os.path.dirname(__file__)
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+ROOT_DIRECTORY = dirname(__file__)
 
 
-def full_path(path):
+def full_path(path=None):
+    if path is None:
+        return ROOT_DIRECTORY
     return ROOT_DIRECTORY + '/' + path
+
+
+def parse_date(date_string):
+    return datetime.strptime(date_string, DATE_FORMAT)
+

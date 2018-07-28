557a321c5c72283f70ffdee6a6bcb24f17e61e9f
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-07-28 15:35:50 +0300

    Attempt fix CI drop update_updated_at_column bug

--- schema/drop.sql
+++ schema/drop.sql
@@ -1,5 +1,5 @@
 drop trigger update_entries_created_at on entries;
 drop trigger update_users_created_at on users;
-drop function update_updated_at_column;
+drop function update_updated_at_column();
 drop table if exists entries;
 drop table if exists users;
